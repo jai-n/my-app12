561bd2c56f4c6e4d18056da8f450f8ff3319025b
nishant-n <cis@CISM467.(none)>
2013-08-03 18:33:17 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jaiapp::Application.config.secret_key_base = '67eae68f88804efc7fc451f3d5a2b3046ddc5c00c001002fabc3a2c8db315dca640a25e8877d8a6429db146ebf5f6d885fccf9591dcb0e8ea106237836d59820'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jaiapp::Application.config.session_store :cookie_store, key: '_jaiapp_session'
